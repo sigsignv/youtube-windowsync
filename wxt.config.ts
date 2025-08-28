9195b500cf533b0ce79dccccf51fd6e0a140a29d
Sigsign <sig@signote.cc>
2025-08-28 15:06:49 +0900

    Update extension name, description and version

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "wxt";
 
 export default defineConfig({
+  manifest: {
+    name: "YouTube WindowSync",
+  },
   modules: ["@wxt-dev/module-solid"],
   srcDir: "src",
 });
